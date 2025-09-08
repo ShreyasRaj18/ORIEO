2f9278444158c620739f9559bbd9f6209866c26c
Tanishq Som <tanishqsom84@gmail.com>
2025-09-09 02:03:10 +0530

    Added SAR models

--- Optical Imagery/main.py
+++ Optical Imagery/main.py
@@ -6,15 +6,13 @@ import cv2 as cv
 file_path = '/DATA/20170504T083011_20170504T083011_T36SWH__ROI_2279.tif'
 
 with rasterio.open(file_path) as src:
-    # Read bands: R=4, G=3, B=2 (Sentinel-2)
     red = src.read(4)
     green = src.read(3)
     blue = src.read(2)
 
-# Stack bands into RGB image
+
 rgb = np.dstack((red, green, blue))
 
-# Normalize for visualization
 rgb_norm = rgb / np.max(rgb)
 
 
